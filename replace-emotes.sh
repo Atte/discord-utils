589c73444782559984d5edd45bf6e1820691db88
Atte Virtanen <aten.email@gmail.com>
2023-06-01 20:55:53 +0300

    cleanup

--- replace-emotes.sh
+++ replace-emotes.sh
@@ -9,12 +9,12 @@ if ! test -d "$folder"; then
     exit 1
 fi
 
-mkdir -p "$folder/uploaded"
+mkdir -p "$folder/uploaded/"
 
 function api {
     local path="$1"
     shift
-    curl --silent --fail --header "Authorization: Bot $DISCORD_TOKEN" --header 'Content-Type: application/json' "$@" "https://discord.com/api/v10/$path"
+    curl --silent --fail --header "Authorization: Bot $DISCORD_TOKEN" --header 'Content-Type: application/json' --header 'X-Audit-Log-Reason: discord-utils/replace-emotes.sh' "$@" "https://discord.com/api/v10/$path"
 }
 
 emotes="$(api "guilds/$DISCORD_GUILD/emojis")"
@@ -22,17 +22,16 @@ emotes="$(api "guilds/$DISCORD_GUILD/emojis")"
 while read -r filename; do
     ext="${filename##*.}"
     name="$(basename "$filename" ".$ext")"
-    echo "$(date) $name" >&2
 
     image="data:image/$ext;base64,$(base64 "$filename")"
     jq --null-input --arg name "$name" --arg image "$image" '{ $name, $image }' |\
-        api "guilds/$DISCORD_GUILD/emojis" -X POST --data-binary @- >/dev/null
+        api "guilds/$DISCORD_GUILD/emojis" -X POST --data-binary @- | jq >&2
     
     while read -r id; do
         api "guilds/$DISCORD_GUILD/emojis/$id" -X DELETE >/dev/null
     done < <(jq --raw-output --arg name "$name" '.[] | select(.name == $name) | .id' <<<"$emotes")
 
     mv "$filename" "$folder/uploaded/"
-
+    echo "$name"
     sleep 10
 done < <(find "$folder/todo/" -type f -name '*.png' -o -name '*.gif' -o -name '*.jpeg')
